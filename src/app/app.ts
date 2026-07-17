68f54d1bb8b50d73c75db714d844175d847a5d3b
rr-sk-dev <rr.sk.dev@gmail.com>
2026-07-17 15:49:18 +0100

    💄 configure tailwindcss

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,7 @@ import { Component, signal } from '@angular/core';
   selector: 'app-root',
   imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('zelvo-fe');
